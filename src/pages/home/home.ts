c7b9f8166d7e2746487b8ae717636ec0427dbdfb
ser0125 <sergio.llanos.garcia@gmail.com>
2017-09-29 23:18:41 -0500

    Consuming the Api of Mercadolibre with Ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+var output = console.log;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  informationItems: any=[];
+  ready: boolean=false;
+   url: any ='https://api.mercadolibre.com/sites/MCO/search?q=';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: Http) {
 
   }
 
+  searchProduct(item: any){
+
+    return new Promise((resolve, reject)=> {
+      var link:string = this.url + item
+      this.http.get(link).map(res=>res.json()).subscribe(
+        (data) => {
+          this.informationItems=data
+          resolve(data)
+         output(this.informationItems)
+        },
+        (error) => {
+          reject("Error: "+ error)
+        })
+      
+})
+
 }
+
+}  
+
+
